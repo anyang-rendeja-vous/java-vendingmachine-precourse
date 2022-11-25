b34028fd66d8271d0d1934169cc722adb9f9e13f
minju412 <ln8847@ajou.ac.kr>
2022-11-25 14:34:31 +0900

    feat: add method to input product
    - 상품을 입력받고 검증하기 위해 메서드 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,5 +1,23 @@
 package vendingmachine.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.util.InputValidator;
+
 public class InputView {
 
+    private static final String ENTER_PRODUCT_INFORMATION = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String PRODUCT_INPUT_FORMAT_ERROR = "[콜라,1500,20];[사이다,1000,10] 과 같은 형식으로 입력해 주세요.";
+
+    private String input() {
+        return Console.readLine();
+    }
+
+    public String inputProduct() {
+        System.out.println(ENTER_PRODUCT_INFORMATION);
+        String products = input();
+        if (!InputValidator.checkProduct(products)) {
+            throw new IllegalArgumentException(PRODUCT_INPUT_FORMAT_ERROR);
+        }
+        return products;
+    }
 }
