262ece82af0192613882c409100bb9c9b6027e08
minju412 <ln8847@ajou.ac.kr>
2022-11-25 15:33:08 +0900

    feat(VendingMachineController): initialize vending machine
    - 자판기의 보유 금액과 동전을 초기화한다.

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,5 +1,31 @@
 package vendingmachine.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import vendingmachine.domain.Product;
+import vendingmachine.domain.VendingMachine;
+import vendingmachine.view.InputView;
+
 public class VendingMachineController {
 
+    private final InputView inputView;
+
+    public VendingMachineController() {
+        inputView = new InputView();
+    }
+
+    public void run() {
+
+        String amountOfMoney = inputView.inputAmountOfMoney();
+
+        String products = inputView.inputProduct();
+        List<Product> productGroup = new ArrayList<>();
+        if (products.contains(";")) {
+            Arrays.asList(products.split(";"))
+                    .forEach(product -> productGroup.add(new Product(product)));
+        }
+
+        VendingMachine vendingMachine = new VendingMachine(amountOfMoney, productGroup);
+    }
 }
