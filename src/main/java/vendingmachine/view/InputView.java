959b71a381496df8ff54f06674d60c5f53159bc6
minju412 <ln8847@ajou.ac.kr>
2022-11-25 16:52:42 +0900

    feat(InputView): add method to input user money
    - 투입 금액을 입력받기 위해

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -7,16 +7,17 @@ public class InputView {
 
     private static final String ENTER_VENDING_MACHINE_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String ENTER_PRODUCT_INFORMATION = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String ENTER_INPUT_MONEY = "투입 금액을 입력해 주세요.";
+    private static final String ENTER_PRODUCT_NAME = "구매할 상품명을 입력해 주세요.";
     private static final String PRODUCT_INPUT_FORMAT_ERROR = "[콜라,1500,20];[사이다,1000,10] 과 같은 형식으로 입력해 주세요.";
 
     private String input() {
         return Console.readLine();
     }
 
-    public String inputAmountOfMoney() {
+    public String inputMachineMoney() {
         System.out.println(ENTER_VENDING_MACHINE_MONEY);
-        String products = input();
-        return products;
+        return input();
     }
 
     public String inputProduct() {
@@ -27,5 +28,16 @@ public class InputView {
             throw new IllegalArgumentException(PRODUCT_INPUT_FORMAT_ERROR);
         }
         return products;
+    }
+
+    public String inputUserMoney() {
+        System.out.println();
+        System.out.println(ENTER_INPUT_MONEY);
+        return input();
+    }
+
+    public String inputProductName() {
+        System.out.println(ENTER_PRODUCT_NAME);
+        return input();
     }
 }
