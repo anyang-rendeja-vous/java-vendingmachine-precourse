802cf2ba30fe889be21bc3cdcf9409263ce4486d
minju412 <ln8847@ajou.ac.kr>
2022-11-25 12:56:40 +0900

    feat(initCoin): add skeleton method
    - 자판기의 금액을 세팅하는 메서드 뼈대 작성

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,5 +1,20 @@
 package vendingmachine.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VendingMachine {
+
+    private final Integer amountOfMoney;
+    private final List<Coin> coins = new ArrayList<>();
+
+    public VendingMachine(Integer amountOfMoney) {
+        this.amountOfMoney = amountOfMoney;
+        initCoin(amountOfMoney);
+    }
+
+    public List<Coin> initCoin(Integer amountOfMoney) {
+        return coins;
+    }
 
 }
