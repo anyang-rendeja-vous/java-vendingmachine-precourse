cbbb13f2f360b7cd9a2da404e011aef0f8f329bf
minju412 <ln8847@ajou.ac.kr>
2022-11-25 15:57:48 +0900

    feat(VendingMachine): count each coin
    - 500원부터 100원까지가 각각 몇 개인지 계산

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -33,4 +33,23 @@ public class VendingMachine {
         }
 
     }
+
+    public List<Integer> countEachCoin() {
+        List<Integer> count = Arrays.asList(0, 0, 0, 0);
+        coinGroup.forEach(coin -> {
+            if (coin.getAmount() == 500) {
+                count.set(0, count.get(0) + 1);
+            }
+            if (coin.getAmount() == 100) {
+                count.set(1, count.get(1) + 1);
+            }
+            if (coin.getAmount() == 50) {
+                count.set(2, count.get(2) + 1);
+            }
+            if (coin.getAmount() == 10) {
+                count.set(3, count.get(3) + 1);
+            }
+        });
+        return count;
+    }
 }
