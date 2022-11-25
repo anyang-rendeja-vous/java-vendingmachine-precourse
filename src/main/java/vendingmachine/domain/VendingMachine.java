7bdec9ec596bb279dd94b40ea6816ca798da05e7
minju412 <ln8847@ajou.ac.kr>
2022-11-25 16:54:46 +0900

    feat(VendingMachine): sell products to user
    - 해당 물건이 있는지 확인한다
      - 있다면 투입 금액을 차감한다
      - 없다면 익셉션 발생

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -8,8 +8,10 @@ import vendingmachine.util.MoneyGenerator;
 
 public class VendingMachine {
 
+    private static final String NOT_EXISTED_PRODUCTION = "존재하지 않는 상품입니다.";
     private final List<Coin> coinGroup = new ArrayList<>();
     private List<Product> products;
+    private Integer inputMoney;
 
     public VendingMachine(String amountOfMoney) {
         initCoin(amountOfMoney);
@@ -17,6 +19,34 @@ public class VendingMachine {
 
     public void setProducts(List<Product> products) {
         this.products = products;
+    }
+
+    public void setInputMoney(int inputMoney) { // TODO: setter
+        this.inputMoney = inputMoney;
+    }
+
+    public boolean sell(String inputProduct) {
+        if (products.stream()
+                .map(Product::getName)
+                .collect(Collectors.toList())
+                .contains(inputProduct)) {
+            Product prod = products.stream()
+                    .filter(product -> product.getName().equals(inputProduct))
+                    .findAny()
+                    .orElse(null);
+            if (prod == null) {
+                throw new IllegalArgumentException(NOT_EXISTED_PRODUCTION);
+            }
+            if (inputMoney >= prod.getPrice()) {
+                inputMoney -= prod.getPrice();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getInputMoney() { // TODO: getter
+        return inputMoney;
     }
 
     public void initCoin(String amountOfMoney) {
