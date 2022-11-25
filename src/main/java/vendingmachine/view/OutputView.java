ed905f193a88a4efeaa274b4dd5fd01f655f48ab
minju412 <ln8847@ajou.ac.kr>
2022-11-25 15:59:41 +0900

    feat(OutputView): add method to print coins
    - 자판기가 가지고 있는 코인을 출력하기 위해

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,5 +1,23 @@
 package vendingmachine.view;
 
+import java.util.List;
+import vendingmachine.domain.VendingMachine;
+
 public class OutputView {
 
+    private static final String VENDING_MACHINE_COINS = "자판기가 보유한 동전";
+    private static final String NUMBER_OF_500 = "500원 - ";
+    private static final String NUMBER_OF_100 = "100원 - ";
+    private static final String NUMBER_OF_50 = "50원 - ";
+    private static final String NUMBER_OF_10 = "10원 - ";
+    private static final String NUMBER = "개";
+
+    public void printVendingMachineCoins(VendingMachine vendingMachine) {
+        System.out.println(VENDING_MACHINE_COINS);
+        List<Integer> coinCounts = vendingMachine.countEachCoin();
+        System.out.println(NUMBER_OF_500 + coinCounts.get(0) + NUMBER);
+        System.out.println(NUMBER_OF_100 + coinCounts.get(1) + NUMBER);
+        System.out.println(NUMBER_OF_50 + coinCounts.get(2) + NUMBER);
+        System.out.println(NUMBER_OF_10 + coinCounts.get(3) + NUMBER);
+    }
 }
