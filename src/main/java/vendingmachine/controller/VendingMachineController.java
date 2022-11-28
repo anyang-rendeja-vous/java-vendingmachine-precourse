2d05c6d040f7ab28ab515e41d120748604e2639d
minju412 <ln8847@ajou.ac.kr>
2022-11-28 17:15:21 +0900

    feat(Controller): receive input repeatedly
    - 에러 발생 지점부터 다시 입력받는다

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -3,6 +3,7 @@ package vendingmachine.controller;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 import vendingmachine.domain.coin.Change;
 import vendingmachine.domain.Product;
 import vendingmachine.domain.VendingMachine;
@@ -20,49 +21,55 @@ public class VendingMachineController {
     }
 
     public void run() {
-        try {
-            String amountOfMoney = inputView.inputMachineMoney(); // 자판기가 가지고 있는 금액
+        String amountOfMoney = repeat(inputView::inputMachineMoney);
 
-            VendingMachine vendingMachine = new VendingMachine(amountOfMoney);
-            outputView.printVendingMachineCoins(vendingMachine);
+        VendingMachine vendingMachine = new VendingMachine(amountOfMoney);
+        outputView.printVendingMachineCoins(vendingMachine);
 
-            String products = inputView.inputProduct();
-            List<Product> productGroup = new ArrayList<>();
-            Arrays.asList(products.split(";"))
-                    .forEach(product -> productGroup.add(new Product(product)));
-            vendingMachine.setProducts(productGroup);
+        String products = repeat(inputView::inputProduct);
 
-            String inputUserMoney = inputView.inputUserMoney(); // 투입 금액
-            vendingMachine.setInputMoney(Integer.parseInt(inputUserMoney));
+        List<Product> productGroup = new ArrayList<>();
+        Arrays.asList(products.split(";"))
+                .forEach(product -> productGroup.add(new Product(product)));
+        vendingMachine.setProducts(productGroup);
 
-            int inputMoney = 0;
-            while (true) {
-                if (vendingMachine.getInputMoney() < vendingMachine.minimumPriceOfProduct()) {
-                    break;
-                }
-                // 모든 상품이 소진된 경우
-                boolean isOutOfStock = false;
-                for (Product product : productGroup) {
-                    if (!product.canBuy()) {
-                        isOutOfStock = true;
-                    }
-                }
-                if (isOutOfStock) {
-                    break;
-                }
-                inputMoney = vendingMachine.getInputMoney();
-                outputView.printInputMoney(inputMoney);
-                String inputProductName = inputView.inputProductName();
-                vendingMachine.sell(inputProductName);
+        String inputUserMoney = repeat(inputView::inputUserMoney);
+
+        vendingMachine.setInputMoney(Integer.parseInt(inputUserMoney));
+
+        int inputMoney = 0;
+        while (true) {
+            if (vendingMachine.getInputMoney() < vendingMachine.minimumPriceOfProduct()) {
+                break;
             }
-
-            Change change = new Change();
-            change.calculateRemainder(vendingMachine);
+            // 모든 상품이 소진된 경우
+            boolean isOutOfStock = false;
+            for (Product product : productGroup) {
+                if (!product.canBuy()) {
+                    isOutOfStock = true;
+                }
+            }
+            if (isOutOfStock) {
+                break;
+            }
+            inputMoney = vendingMachine.getInputMoney();
             outputView.printInputMoney(inputMoney);
-            outputView.printRemainder(change);
+            String inputProductName = inputView.inputProductName();
+            vendingMachine.sell(inputProductName);
+        }
 
+        Change change = new Change();
+        change.calculateRemainder(vendingMachine);
+        outputView.printInputMoney(inputMoney);
+        outputView.printRemainder(change);
+    }
+
+    private <T> T repeat(Supplier<T> inputReader) {
+        try {
+            return inputReader.get();
         } catch (IllegalArgumentException ex) {
             outputView.printError(ex.getMessage());
+            return repeat(inputReader);
         }
     }
 }
