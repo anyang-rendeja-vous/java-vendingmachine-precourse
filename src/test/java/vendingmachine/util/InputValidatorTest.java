fbc1fb6a86f38d4623fb80972699091ef44c1346
minju412 <ln8847@ajou.ac.kr>
2022-11-28 16:57:07 +0900

    test(InputValidatorTest): add tests

--- src/test/java/vendingmachine/util/InputValidatorTest.java
+++ src/test/java/vendingmachine/util/InputValidatorTest.java
@@ -1,6 +1,6 @@
 package vendingmachine.util;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,18 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class InputValidatorTest {
 
-    @DisplayName("물품이 올바른 형식이면 true 를 반환합니다.")
-    @ParameterizedTest
-    @CsvSource({
-            "'[콜라,1500,20];[사이다,1000,10]'",
-            "'[콜라,1500,20]'"
-    })
-    void inputValidator_valid(String product) {
-        assertThat(InputValidator.isFormatValid(product))
-                .isTrue();
-    }
-
-    @DisplayName("물품이 올바른 형식이 아니면 false 를 반환합니다.")
+    @DisplayName("물품이 올바른 형식이 아니면 예외가 발생합니다.")
     @ParameterizedTest
     @CsvSource({
             "'[콜라,1500,20][사이다,1000,10]'",
@@ -27,7 +16,8 @@ class InputValidatorTest {
             "'[콜라,1500,20'"
     })
     void inputValidator_invalid(String product) {
-        assertThat(InputValidator.isFormatValid(product))
-                .isFalse();
+        InputValidator inputValidator = new InputValidator();
+        assertThatThrownBy(() -> inputValidator.isFormatValid(product))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
